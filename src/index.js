17b6703806d504779c8530e70ca935bb043bf3d6
Gemi Yudhia <githuba122@gmail.com>
2025-08-07 16:36:23 +0700

    memahami konsep middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const usersRoutes = require("./routes/users");
 
+const middlewareLogRequest = require("./middleware/logs");
+
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("Hello World");
-// });
+app.use(middlewareLogRequest);
 
 app.use("/users", usersRoutes);
 
